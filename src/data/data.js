ee48a11948a34023b452506c4c468c705bf20c38
Samet Orbuk <orbuk60@gmail.com>
2024-08-08 13:56:53 +0300

    düzenleme

---
+++ src/data/data.js
@@ -0,0 +1,60 @@
+export const introCompData = {username: "SAMET",
+    title:"Creative Thinker Minimalizm Lover",
+    explanation:"Hi, I’m Almila. I’m a full-stack developer. If you are looking for a Developer who to craft solid and scalable frontend products with great user experiences. Let’s shake hands with me.",
+    img:"./src/images/user-img.jpg",
+    pathToGithub:"https://github.com/sametorbuk?tab=repositories",
+    pathToLinkedin:"https://www.linkedin.com/in/sametorbuk/"
+}
+
+
+
+export const skillsData = {
+    javaScript:{
+        skillName:"Java Script",
+        explanation:"JavaScript, geliştiricilerin etkileşimli web sayfaları oluşturmak için kullandığı bir programlama dilidir."
+    },
+    react:{
+        skillname:"React JS",
+        explanation:"React kullanıcı arayüzü oluşturmaya yarayan açık kaynak kodlu bir javascript kütüphanesidir. Facebook önderliğinde bir geliştirici grubu tarafından geliştirilmekte olan React, Model-View-Controller prensibine uygun olarak oluşturulmuştur."
+    },
+
+    nodeJs:{
+        skillname:"Node JS",
+        explanation:"Node.js, açık kaynaklı, genelde sunucu tarafında çalışan ve ağ bağlantılı uygulamalar için geliştirilmiş bir çalıştırma ortamıdır. Node.js uygulamaları genelde istemci tarafı betik dili olan JavaScript kullanılarak geliştirilir."
+    }
+}
+
+
+
+
+export const profileData = {
+    birthDate : "09.07.2000",
+    city:"Mersin",
+    education:"Dokuz Eylül Üni. Sınıf Öğretmenliği/Lisans",
+    PreferRole:"Full Stack Developer",
+    aboutUser:"A curious and hardworking software developer who wants to keep up with the ever-advancing technology"
+}
+
+
+
+
+export const ProjectsData = [{
+
+
+
+
+
+},{
+
+
+
+
+
+
+},{
+
+  
+    
+    
+
+}]
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,18 +1,58 @@
 import axios from "axios";
+import { useState } from "react";
+
+
+const METHODS = {
+    PUT:"put",
+    GET:"get",
+    POST:"POST",
+    DELETE:"delete"
+}
 
 
 
 
-export default function useAxios({initialData , baseUrl =}){
+
+export default function useAxios({initialData }){
+const [data , setData]=useState(initialData)
+const [loading , setLoading]= useState(false)
+const [err , setErr]=useState(false)
+
+
+
+const instance = axios.create({
+    baseURL: "https://reqres.in/api/workintech",
+    timeout: 1000,
+    headers: {'Autoauthentication': ""}
+  });
+
+
+
+  const MakeRequest = ({url,
+   method,
+   data = null
+ })=>{
+
+  setLoading(true)
+
+  instance[method](url , data ? data : null)
+  .then((response)=>{
+    setData(response.data)
+    setLoading(false)
+  })
+  .catch((err)=>{
+    setErr(!err)
+  })
 
 
 
 
 
+}
 
 
 
-
+return {data , MakeRequest , setData , METHODS }
 
 
 
